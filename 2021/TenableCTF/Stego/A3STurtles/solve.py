05eae2781aa53757f5cfcc5a860f15fe589e60d9
Nicolas Fiset <57725357+inho28@users.noreply.github.com>
2021-02-23 07:59:23 -0500

    Update solve.py

--- 2021/TenableCTF/Stego/A3STurtles/solve.py
+++ 2021/TenableCTF/Stego/A3STurtles/solve.py
@@ -3,37 +3,26 @@
 import sys
 import zipfile
 
-ouput = ""
+
 def unzip(file_name):
     with zipfile.ZipFile(file_name) as myzip:
         out=[]
         file = myzip.namelist()[0]
         try:
             password = b'0'
-            myzip.extractall('out', pwd=password)
-        
+            myzip.extractall('out', pwd=password)       
             if '.zip' in file:
                 print("0")
-                #print('Unzipping, '+file+ ' with 0')
-                #global output(append("1"))
-                
                 unzip('out/' + file)
-                out.append("0")
-                
-
+                               
         except:
             password = b'1'
-            myzip.extractall ('out', pwd=password)
-            
+            myzip.extractall ('out', pwd=password)            
             if '.zip' in file:
                 print("1")
-                #print('Unzipping, '+file+ 'with 1')
                 unzip('out/' + file)
-                out.append("1")
-    
+                
     return(out)
         
 
-
 output = unzip('turtles128.zip')
-print(output)
